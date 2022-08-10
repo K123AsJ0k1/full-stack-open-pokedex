f4603b06b03bb1283b0723d8e74ac716a1e3c869
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-08-10 14:21:41 +0300

    Test retry 4

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  setupNodeEvents(on, config) {
+    // implement node event listeners here
+  },
+
   e2e: {
+    pageLoadTimeout: 200000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
